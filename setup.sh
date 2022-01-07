4bdc733c01f36164cc64f7b2c9f7205cd1f93f9c
anastasia-pashkova <anastasia.pashkova@gmail.com>
2022-01-07 15:02:36 -0800

    changed email to mine

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"michaelc@thedataincubator.com\"\n\
+email = \"anastasia.pashkova@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
